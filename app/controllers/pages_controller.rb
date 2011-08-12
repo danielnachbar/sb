1a3385becf058bec60e3f56f2b885584c3700c9a
Dan Nachbar <dan@nachbar.com>
2011-08-12 14:33:49 -0400

    now have stylesheets and some basic formatting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "SpiffyBooks Home"
   end
 
   def contact
+    @title = "SpiffyBooks Contact"   
   end
 
   def about
+    @title = "About SpiffyBooks"    
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 
 describe PagesController do
 
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
+    # it "should have the right title" do
+    #   get 'home'
+    #   response.should have_selector("title", :content => /home/i)
+    # end
   end
 
   describe "GET 'contact'" do
