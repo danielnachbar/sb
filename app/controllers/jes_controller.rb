6097c68be6a822a1139affa3a84b206f4656ba5d
Dan Nachbar <dan@nachbar.com>
2011-09-01 15:05:07 -0400

    added create action and tests

--- app/controllers/jes_controller.rb
+++ app/controllers/jes_controller.rb
@@ -3,4 +3,16 @@ class JesController < ApplicationController
     @title = "New Journal Entry"
   end
 
+  def create
+    @je = Je.new(params[:je])
+
+    if (@je.save)                            
+      redirect_to je_path(@je.id), :flash => { :success => "Journal Entry Created!" }
+    else         
+      @title = "New Journal Entry"
+      render :new  
+    end      
+  end
+
+
 end
--- spec/controllers/jes_controller_spec.rb
+++ spec/controllers/jes_controller_spec.rb
@@ -15,6 +15,74 @@ describe JesController do
       response.should have_selector('title', :content => "New Journal Entry")
     end          
          
-  end
+  end    # GET 'new'
+  
+
+  describe "POST 'create'" do 
+
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :credit => 0, :debit => 0, :amount => -1 }
+      end
+                        
+      it "should render the 'new' page" do
+        post :create, :je => @attr
+        response.should render_template(:new)
+      end            
+      
+      it "should have the right title" do
+        post :create, :je => @attr
+        response.should have_selector('title', :content => "New Journal Entry")
+      end           
+      
+      it "should not create an account" do  
+        lambda do
+          post :create, :je => @attr
+        end.should change(Je, :count).by(0)
+      end                          
+      
+        
+    end      # create failure  
+
+             
+    describe "success" do
+      
+      before(:each) do
+        @a1 = Account.create!(:name => "test1", :atype => "asset"    )
+        @a2 = Account.create!(:name => "test2", :atype => "liability")
+        @a3 = Account.create!(:name => "test3", :atype => "income"   )
+        @a4 = Account.create!(:name => "test4", :atype => "expense"  )          
+
+        @attr = { 
+          :credit => @a1.id,
+          :debit => @a2.id,
+          :amount =>  300,         
+          :comment => "I like turtles!"
+        }
+      end
+          
+      it "should create an account" do
+        lambda do
+          post :create, :je => @attr
+        end.should change(Je, :count).by(1)
+      end
+         
+               
+      it "should redirect to the account show page" do
+        post :create, :je => @attr
+        response.should redirect_to(je_path(assigns(:je)))
+      end             
+      
+      it "should have a flash message" do
+        post :create, :je => @attr
+        flash[:success].should =~ /Created/i
+      end                      
+    end               # success                  
+    
+                                                             
+ 
+    
+  end      # POST create  
 
 end
